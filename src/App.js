b5423cf1a8efb134b70e5a31f767b2f1391894a8
Vinicius Brenner <vinicius_brennerr@hotmail.com>
2020-10-14 10:25:30 -0300

    Material UI

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import './App.css';
 import FormularioCadastro from './Components/FormularioCadastro/FormularioCadastro';
+import { Container, Typography } from "@material-ui/core"
+import 'fontsource-roboto';
 
 function App() {
   return (
-    <Fragment>
-      <h1>Formulário de Cadastro</h1>
+    <Container component="article" maxWidth="sm">
+      <Typography variant="h3" component="h1" align="center">Formulário de Cadastro</Typography>
       <FormularioCadastro />
-    </Fragment>
+    </Container>
   );
 }
 
--- src/Components/FormularioCadastro/FormularioCadastro.jsx
+++ src/Components/FormularioCadastro/FormularioCadastro.jsx
@@ -1,13 +1,28 @@
 import React from 'react';
+import { Button, TextField, Switch, FormControlLabel } from "@material-ui/core";
 
 function FormularioCadastro() {
 
-    return(
+    return (
         <form>
-            <input type="text"/>
+            <TextField id="nome" label="Nome" variant="outlined" fullWidth margin="normal" />
+            <TextField id="sobrenome" label="Sobrenome" variant="outlined" fullWidth margin="normal" />
+            <TextField id="CPF" label="CPF" variant="outlined" fullWidth margin="normal" />
+
+            <FormControlLabel label="Promoções" control={
+                <Switch name="promocoes" defaultchecked color="primary" />
+            } />
+
+            <FormControlLabel label="Novidades" control={
+                <Switch name="novidades" defaultchecked color="primary" />
+            } />
+
+            <Button type="submit" variant="contained" color="primary">
+                Cadastrar
+            </Button>
         </form>
     );
-    
+
 }
 
 export default FormularioCadastro;
